2430258e181b961b088924d145d53f3be3574e09
Kyoko <kyokoconnect@gmail.com>
2021-04-11 10:50:44 -0400

    first comm, complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = function(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email
+          })
+        })
+.then(function(responce) {
+    return responce
+    })
+.then(function(object) {
+    document.body.innerHTML = object[ "id" ]
+})
+.catch(function(error) {
+    document.body.innerHTML = error.message
+})
+}
+
+console.log(submitData)
+
+
